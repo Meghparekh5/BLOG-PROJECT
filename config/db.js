8aad16587522af90f1ab10da4fc288576f3bb525
Meghparekh5 <parekhmegh155@gmail.com>
2026-06-29 11:38:28 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/adminPanel');
+mongoose.connect('mongodb+srv://parekhmegh155_db_user:Cookie11@admin-management.fa6kttj.mongodb.net/?appName=admin-management');
 
 const db = mongoose.connection;
 
